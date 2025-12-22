4e8c918eae41efed78203e987120a06df992f9dd
manikandan <manikandan110305@gmail.com>
2025-12-23 00:46:21 +0530

    update: improved Login UI/UX and removed bg image

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,12 +13,15 @@ import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 
+const allowedOrigin = process.env.APPLICATION_URL || "http://localhost:5173";
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigin,
     credentials: true,
   })
 );
+
 
 app.use(express.json());
 app.use(cookieParser());
